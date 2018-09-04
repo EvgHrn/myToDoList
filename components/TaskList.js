04494b701b2bbf0baa7a76a8ca88711553647962
EvgHrn <evg.hrn@gmail.com>
2018-09-04 13:25:39 +0400

    Make list of tasks as RN FlatList

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 import { Task } from './Task'
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
 
 export class TaskList extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log(props);
+        
+    }
+
+    _renderItem = (task) => {
+        return (
+            <Task key={task.item.hash} taskText={task.item.text + ' ' + task.item.hash}/>
+        );
+    }
+
+    _keyExtractor = (item) => String (item.hash);
 
     render() {
 
         return (
             <View>
-                {this.props.taskList.map((task) => {
-                    return (
-                        <Task key={task.hash} taskText={task.text + ' ' + task.hash}/>
-                    );
-                })}
+                <FlatList
+                    data={this.props.taskList}
+                    keyExtractor={this._keyExtractor}
+                    renderItem={this._renderItem}
+                />
             </View>
 
         )
